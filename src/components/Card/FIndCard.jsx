a848e0d9f67e1f4d2eba09fce357562163a654f9
PANJI ADHIPRAMANA <panjiadhipramana@gmail.com>
2022-05-27 02:43:50 +0800

    upload

--- src/components/Card/FIndCard.jsx
+++ src/components/Card/FIndCard.jsx
@@ -1,58 +1,109 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { filterByInput } from "../../store/actions/CarActions";
 
 class FIndCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      capacity: "",
+      tipeDriver: "-1",
+      dateTime: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(this.state);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+
+    if (target.name === "jumlah-penumpang") {
+      if (isNaN(value)) {
+        return;
+      }
+      this.setState({
+        capacity: value,
+      });
+    }
+
+    if (target.name === "tipe-driver") {
+      this.setState({
+        tipeDriver: value,
+      });
+    }
+
+    if (target.name === "tanggal") {
+      this.setState({
+        dateTime: value,
+      });
+    }
+  }
+
   render() {
     return (
       <>
-        <div class="container-sm card-cari-cars">
-          <div class="row row-cari-cars">
-            <div class="col-lg-11 col-12">
-              <div class="row">
-                <div class="col-lg-3 col-6 card-cari">
+        <div className="container-sm card-cari-cars">
+          <div className="row row-cari-cars">
+            <div className="col-lg-11 col-12">
+              <div className="row">
+                <div className="col-lg-3 col-6 card-cari">
                   <p>Tiper Driver</p>
-                  <select name="cars" id="tipe-driver" class="form-select menu">
-                    <option value="" selected>
+                  <select
+                    name="tipe-driver"
+                    id="tipe-driver"
+                    className="form-select menu"
+                    onChange={(e) => this.handleInputChange(e)}
+                    value={this.state.tipeDriver}
+                  >
+                    <option value="-1" selected>
                       Pilih Tipe Driver
                     </option>
                     <option value="1">Dengan Sopir</option>
                     <option value="0">Tanpa Sopir (Lepas Kuncil)</option>
                   </select>
                 </div>
-                <div class="col-lg-3 col-6 card-cari">
+                <div className="col-lg-3 col-6 card-cari">
                   <p>Tanggal</p>
                   <input
                     type="date"
                     id="tanggal"
                     name="tanggal"
-                    class="form-control menu"
+                    className="form-control menu"
+                    onChange={(e) => this.handleInputChange(e)}
                   />
                 </div>
-                <div class="col-lg-3 col-6 card-cari">
+                <div className="col-lg-3 col-6 card-cari">
                   <p>Waktu Jemput/Ambil</p>
                   <input
                     type="time"
                     id="waktu-ambil"
                     name="waktu"
-                    class="form-control menu"
+                    className="form-control menu"
                   />
                 </div>
-                <div class="col-lg-3 col-6 card-cari">
+                <div className="col-lg-3 col-6 card-cari">
                   <p>Jumlah Penumpang (optional)</p>
                   <input
                     type="text"
                     id="jumlah-penumpang"
                     name="jumlah-penumpang"
-                    class="form-control menu"
+                    className="form-control menu"
                     placeholder="Jumlah Penumpang"
+                    value={this.state.capacity}
+                    onChange={(e) => this.handleInputChange(e)}
                   />
                 </div>
               </div>
             </div>
-            <div class="col-lg-1 col-12 btn-cari-mobil">
+            <div className="col-lg-1 col-12 btn-cari-mobil">
               <button
                 id="cari-mobil"
                 type="button"
-                class="btn btn-primary btn-register"
+                className="btn btn-primary btn-register"
+                onClick={() => this.props.onFindCar(this.state)}
               >
                 Cari Mobil
               </button>
@@ -64,4 +115,12 @@ class FIndCard extends Component {
   }
 }
 
-export default FIndCard;
+function mapDispatchToProps(dispatch) {
+  return {
+    onFindCar: (data) => {
+      dispatch(filterByInput(data));
+    },
+  };
+}
+
+export default connect(null, mapDispatchToProps)(FIndCard);
